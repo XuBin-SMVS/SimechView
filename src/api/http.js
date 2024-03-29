a654285b78ea03c12cfe37f216685be5698c1ce2
[XuBin-SMVS] <xu.xb.bin@siemens.com>
2024-03-29 16:46:03 +0800

    2024-3-29-001

---
+++ src/api/http.js
@@ -0,0 +1,27 @@
+import axios from 'axios'
+import qs from 'qs'
+console.log(process.env.NODE_ENV)
+switch (process.env.NODE_ENV.toLowerCase()) {
+    case "production":
+        axios.defaults.baseURL = "http://10.192.24.233:443/"   // "http://192.168.3.254:443/"  // "http://10.192.24.233:443/"
+        break;
+    case "development":
+        axios.defaults.baseURL = "http://192.168.3.254:1000/"
+        break;
+    // default:
+    //     axios.defaults.baseURL = "http://127.0.0.1"
+}
+
+axios.defaults.timeout = 0;
+axios.defaults.transformRequest = data => qs.stringify(data)
+
+axios.interceptors.response.use (response=>{
+    // console.log(response)
+    console.log(process.env.NODE_ENV)
+    return response.data;
+
+}, error=>{ 
+    return Promise.reject(error)
+})
+
+export default axios
